82df8b9159a4fda3c3ab50a25feb75c4179035fb
Lairita0721 <rita950721@gmail.com>
2026-07-17 22:03:44 +0800

    Update UVA12019.py

--- UVA12019.py
+++ UVA12019.py
@@ -3,31 +3,31 @@ for i in range(n):
     md = list(map(int, input().split()))
     mouth = md[0]
     date = md[1]
-    date += 7
+    date += 21
     if mouth == 1:
         day = (date - 10) % 7
     elif mouth == 2:
-        day = abs(date - 21) % 7
+        day = (date - 21) % 7
     elif mouth == 3:
-        day = abs(date - 7) % 7
+        day = (date - 7) % 7
     elif mouth == 4:
-        day = abs(date - 4) % 7
+        day = (date - 4) % 7
     elif mouth == 5:
-        day = abs(date - 9) % 7
+        day = (date - 9) % 7
     elif mouth == 6:
-        day = abs(date - 6) % 7
+        day = (date - 6) % 7
     elif mouth == 7:
-        day = abs(date - 11) % 7
+        day = (date - 11) % 7
     elif mouth == 8:
-        day = abs(date - 8) % 7
+        day = (date - 8) % 7
     elif mouth == 9:
-        day = abs(date - 5) % 7
+        day = (date - 5) % 7
     elif mouth == 10:
-        day = abs(date - 10) % 7
+        day = (date - 10) % 7
     elif mouth == 11:
-        day = abs(date - 7) % 7
+        day = (date - 7) % 7
     else:
-        day = abs(date - 12) % 7
+        day = (date - 12) % 7
     
     if day == 0:
         print("Monday")
