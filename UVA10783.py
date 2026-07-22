f405aa4e4c6883220ca58e18011c8fe95b7f8337
Lairita0721 <rita950721@gmail.com>
2026-07-22 21:48:26 +0800

    Update UVA10783.py

--- UVA10783.py
+++ UVA10783.py
@@ -3,8 +3,15 @@ l = []
 for i in range(t):
     a = int(input())
     b = int(input())
-    n = (b - a) // 2 + 1
-    ans = n * (a + b) // 2
+    if a % 2 == 0:
+        a += 1
+    if b % 2 == 0:
+        b -= 1
+    if a <= b:
+        n = (b - a) // 2 + 1
+        ans = n * (a + b) // 2
+    else:
+        ans = 0
     l.append(ans)
 for j in range(len(l)):
     index = j + 1
