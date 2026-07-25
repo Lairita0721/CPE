362d9dc0fa79368b0bf16f939661dd3ab89362b5
Lairita0721 <rita950721@gmail.com>
2026-07-25 22:53:31 +0800

    Create UVA11063.py

---
+++ UVA11063.py
@@ -0,0 +1,38 @@
+c = 1
+while(True):
+    try:
+        line = input()
+        if line =="":
+            continue
+        n = int(line)
+        ##if len(n) == 0:
+            ##break
+        summ = set()
+        b2 = True
+        num = list(map(int, input().split()))
+        for i in range(n):
+            if num[i] <= 0:
+                b2 = False
+
+        for i in range(n-1):
+            if num[i] >= num[i+1]:
+                b2 = False
+        for i in range(n):
+            for j in range(i , n):
+                if b2 == False:
+                    break
+                a = num[i] + num[j]
+                if a not in summ:
+                    summ.add(a)
+                else:
+                    b2 = False
+                    break
+        if b2 == True:
+            print(f"Case #{c}: It is a B2-Sequence.", end = "\n")
+            ##print(sum)
+        else:
+            print(f"Case #{c}: It is not a B2-Sequence.", end = "\n")
+            ##print(sum)
+        c += 1
+    except EOFError:
+        break
