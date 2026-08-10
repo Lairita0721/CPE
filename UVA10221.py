4e0d279626071cfd2f7b080f7d578647d0b17f84
Lairita0721 <rita950721@gmail.com>
2026-08-10 23:20:59 +0800

    Update UVA10221.py

--- UVA10221.py
+++ UVA10221.py
@@ -5,8 +5,12 @@ while (True):
         s = int(l[0]) + 6440
         a = int(l[1])
         d = str(l[2])
+        if a > 360:
+            a = a % 360
+        if a > 180:
+            a = 360 - a
         if d == 'min':
-            a = a / 60
+            a = a / 60.0
         arc = s * 2 *  math.pi * (a / 360)
         chord = (s * math.cos(math.radians(90 - a / 2))) * 2
         print(f"{arc:.6f} {chord:.6f}", end = "\n")
