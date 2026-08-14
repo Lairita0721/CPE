882f51525c93d01084a29d1f963fd1e76c86b071
Lairita0721 <rita950721@gmail.com>
2026-08-14 22:32:00 +0800

    Update UVA10242.py

--- UVA10242.py
+++ UVA10242.py
@@ -1,6 +1,8 @@
 while (True):
     try:
         l = list(map(float, input().split()))
+        if not l:
+            continue
         x1 = l[0]
         y1 = l[1]
         x2 = l[2]
@@ -9,14 +11,8 @@ while (True):
         y3 = l[5]
         x4 = l[6]
         y4 = l[7]
-        if x1 == x2 and y1 == y2:
-            b1 = x1
-            b2 = y1
-            a1 = x3
-            a2 = y3
-            c1 = x4
-            c2 = y4
-        elif x1 == x3 and y1 == y3:
+        
+        if x1 == x3 and y1 == y3:
             b1 = x1
             b2 = y1
             a1 = x2
@@ -37,20 +33,14 @@ while (True):
             a2 = y1
             c1 = x4
             c2 = y4
-        elif x2 == x4 and y2 == y4:
+        else:
             b1 = x2
             b2 = y2
             a1 = x3
             a2 = y3
             c1 = x1
             c2 = y1
-        elif x3 == x4 and y3 == y4:
-            b1 = x3
-            b2 = y3
-            a1 = x2
-            a2 = y2
-            c1 = x4
-            c2 = y4
+        
         ans1 = a1 + c1 - b1
         ans2 = a2 + c2 - b2
         print(f"{ans1:.3f} {ans2:.3f}", end = "\n")
