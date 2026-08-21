412274d74199d52e1a3e06d1ee861016a5ba236d
Lairita0721 <rita950721@gmail.com>
2026-08-21 23:30:16 +0800

    Create UVA - 10415.py

---
+++ UVA - 10415.py
@@ -0,0 +1,28 @@
+t = int(input())
+finger = {
+    'c':[2,3,4,7,8,9,10],
+    'd':[2,3,4,7,8,9],
+    'e':[2,3,4,7,8],
+    'f':[2,3,4,7],
+    'g':[2,3,4],
+    'a':[2,3],
+    'b':[2],
+    'C':[3],
+    'D':[1,2,3,4,7,8,9],
+    'E':[1,2,3,4,7,8],
+    'F':[1,2,3,4,7],
+    'G':[1,2,3,4],
+    'A':[1,2,3],
+    'B':[1,2]
+}
+for _ in range(t):
+    song = input()
+    c = [0] * 10
+    last = []
+    for note in song:
+        now = finger[note]
+        for f in now:
+            if f not in last:
+                c[f - 1] += 1
+        last = now
+    print(*c, end = "\n")
